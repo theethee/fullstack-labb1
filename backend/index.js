0e6eea886c63f0b34a8adf8fab397142f608507b
Thitisuda Mahasrithanet <thiti.m90@gmail.com>
2023-11-04 14:31:17 +0100

    Add first version

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,32 @@ const express = require("express"),
 
 const app = express();
 
-app.get("/api", (_request, response) => {
-  response.send({ hello: "JSU22" });
-});
-// // serveras som statiska filer av express i public
-app.use(express.static(path.join(path.resolve(), "dist")));
+// Importerar dotenv och pg
+const dotenv = require("dotenv"),
+  { Client } = require("pg");
 
-app.listen(3000, () => {
-  console.log("Redo på http://localhost:3000/");
+// Hämtar PGURI
+dotenv.config();
+
+// Ansluter till PostgreSQL
+// client.query används för att interagera med databasen
+const client = new Client({
+  connectionString: process.env.PGURI,
+});
+
+client.connect();
+
+// endpoint produkter
+app.get("/api", async (_request, response) => {
+  const { rows } = await client.query("SELECT * FROM beauty_products");
+
+  response.send(rows);
+});
+
+app.use(express.static(path.join(path.resolve(), "public")));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Redo på http://localhost:${port}/`);
 });
